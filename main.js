1d42cc175e2f63090fbb03d68386134ab19fb7c7
dtaskoff <danko@abv.bg>
2014-04-27 00:10:51 +0300

    Fixed app.get() calls from main module

--- main.js
+++ main.js
@@ -7,21 +7,15 @@ function sendJson(res, content) {
     res.send(content);
 }
 
-function getAll() {
-    app.get("/tasks", function (req, res) {
-        sendJson(res, fetch.allJsons());
-        console.log(fetch.allJsons())
-    });
-}
+app.get("/tasks", function (req, res) {
+    sendJson(res, fetch.allJsons());
+    console.log(fetch.allJsons())
+});
 
-function getByTags() {
-    app.get("/tasks", function (req, res) {
-        var tags = req.query["tag"];
-        if (!Array.isArray(tags)) tags = [tags];
-        sendJson(res, fetch.jsonsByTags(tags));
-    });
-}
-
-getAll();
+app.get("/tasks/filter", function (req, res) {
+    var tags = req.query["tag"];
+    if (!Array.isArray(tags)) tags = [tags];
+    sendJson(res, fetch.jsonsByTags(tags));
+});
 
 app.listen(3000);
